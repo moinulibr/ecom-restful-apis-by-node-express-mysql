5429ae8e9f8e2583b19023b66b08ba0efefc785f
moinul <moinulibr@gmail.com>
2026-05-26 23:59:12 +0600

    db config js file modify

---
+++ src/config/db.config.js
@@ -0,0 +1,14 @@
+//db config
+module.exports = {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "",
+    DB: "ecommerce_db",
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+}
