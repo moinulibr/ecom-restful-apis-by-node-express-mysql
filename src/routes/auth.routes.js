b6bd2aa17b87d2d25e62fafd078fced4958aa13a
moinul <moinulibr@gmail.com>
2026-06-09 23:44:28 +0600

    logout route define

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -7,7 +7,7 @@ const { verifyToken, restrictTo } = require('../middlewares/auth.middleware');
 router.post('/register', authController.register); // note: POST /api/v1/auth/register
 router.post('/login', authController.login);       // note: POST /api/v1/auth/login
 
-//logut
+
 //reset password
 //forget password
 router.post('/refresh-token', authController.refreshToken);
@@ -19,4 +19,12 @@ router.get('/admin-dashboard', verifyToken, restrictTo('admin'), (req, res) => {
     });
 });
 
+
+router.post('/logout', verifyToken, (req, res) => {
+    //logut
+    res.status(200).json({
+        status: 'success',
+        message: 'Logout successful!'
+    });
+});
 module.exports = router;
